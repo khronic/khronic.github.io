37691134f9d365e9dc5b11df1eb24d0dc7e3d1b2
khronic <khronic.dev@gmail.com>
2016-09-29 09:21:25 -0700

    Updates

---
+++ app.278b7722b52a9674914e.js
@@ -0,0 +1,2 @@
+webpackJsonp([0,3],{0:function(n,e,o){o(1);var t=o(30);document.body.appendChild(t()),console.log("test")},30:function(n,e){n.exports=function(){var n=document.createElement("h1");return n.className="pure-button",n.innerHTML="UnBooked",n}}});
+//# sourceMappingURL=app.278b7722b52a9674914e.js.map
